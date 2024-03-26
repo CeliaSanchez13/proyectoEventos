d1382f0e1ee0aaab543f0bfbd5d740dfbb74c1c5
Celia Sánchez <celiasanchez4ac@gmail.com>
2024-03-26 19:34:38 +0100

    primera prueba para la recogida de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicesService } from './services/services.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'proyectoEventos';
+export class AppComponent implements OnInit{
+  //Primera prueba para la recuperacion de los datos
+  events!: any[];
+
+  constructor(private _eventService: ServicesService) {}
+
+  ngOnInit(): void {
+    this._eventService.getEvents().subscribe(data => {
+      this.events = data;
+      console.log(this.events)
+    });
+  }
 }
---
+++ src/app/services/services.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(): Observable<any[]> {
+    return this.http.get<any[]>('assets/data/events.json'); // Traemos la estructura JSON
+  }
+  
+}
